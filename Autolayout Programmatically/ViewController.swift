b4c4932affc2227380896e65ac0ae613a14c866f
joricky91 <jonathanricky91@gmail.com>
2023-01-30 10:01:48 +0700

    Initial Commit

---
+++ Autolayout Programmatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout Programmatically
+//
+//  Created by Jonathan Ricky Sandjaja on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
