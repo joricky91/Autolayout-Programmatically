2dacf23542d16bdd67699ac8cd01ab6192341eeb
joricky91 <jonathanricky91@gmail.com>
2023-01-30 10:54:34 +0700

    feat: setup movieBackdrop and constraint

--- Autolayout Programmatically/ViewController.swift
+++ Autolayout Programmatically/ViewController.swift
@@ -17,16 +17,26 @@ class ViewController: UIViewController {
         setupConstraints()
     }
     
-    var movieTitle: UILabel = {
+    lazy var movieTitle: UILabel = {
         let title = UILabel()
         title.text = "Kimi no na Wa"
         title.font = UIFont.systemFont(ofSize: 22, weight: .bold)
         title.translatesAutoresizingMaskIntoConstraints = false
         return title
     }()
+    
+    lazy var movieBackdrop: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "kimi_no_na_wa")
+        image.layer.masksToBounds = true
+        image.layer.cornerRadius = 10
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
 
     func setupViews() {
         view.addSubview(movieTitle)
+        view.addSubview(movieBackdrop)
     }
     
     func setupConstraints() {
@@ -35,6 +45,11 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             movieTitle.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
             movieTitle.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            
+            movieBackdrop.topAnchor.constraint(equalTo: movieTitle.bottomAnchor, constant: 16),
+            movieBackdrop.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            movieBackdrop.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            movieBackdrop.heightAnchor.constraint(equalTo: safeArea.heightAnchor, multiplier: 0.28)
         ])
     }
 }
