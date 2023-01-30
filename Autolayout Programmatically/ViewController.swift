2d0b0d156fdbbe889b650be55f1759b9e6b173b4
joricky91 <jonathanricky91@gmail.com>
2023-01-30 10:41:02 +0700

    feat: setup movieTitle and constraint

--- Autolayout Programmatically/ViewController.swift
+++ Autolayout Programmatically/ViewController.swift
@@ -12,9 +12,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemTeal
+        view.backgroundColor = .white
+        setupViews()
+        setupConstraints()
     }
+    
+    var movieTitle: UILabel = {
+        let title = UILabel()
+        title.text = "Kimi no na Wa"
+        title.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        return title
+    }()
 
-
+    func setupViews() {
+        view.addSubview(movieTitle)
+    }
+    
+    func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            movieTitle.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            movieTitle.topAnchor.constraint(equalTo: safeArea.topAnchor),
+        ])
+    }
 }
 
